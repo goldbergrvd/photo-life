08d19ed183a931d0b055742c032f5621da83ed0a
Hubert Lu <goldbergrvd@gmail.com>
2022-04-27 22:14:35 +0800

    Photo browse slide code clean

--- src/components/PhotoBrowse.tsx
+++ src/components/PhotoBrowse.tsx
@@ -13,8 +13,10 @@ export interface Props {
 }
 
 let _slider: HTMLDivElement
-const PREV_PHOTO_LEFT = -16
-const NEXT_PHOTO_LEFT = -window.innerWidth * 2 - 16 * 5
+const FONT_SIZE = () => parseFloat(getComputedStyle(document.documentElement).fontSize)
+const PREV_PHOTO_LEFT = () => -window.innerWidth * 0 - FONT_SIZE() * 1
+const CURR_PHOTO_LEFT = () => -window.innerWidth * 1 - FONT_SIZE() * 3
+const NEXT_PHOTO_LEFT = () => -window.innerWidth * 2 - FONT_SIZE() * 5
 const TRANSITION_LEFT = 'left .1s ease'
 const TRANSITION_TOP = 'top .1s ease'
 const REVERT_POSITION_TIME = 100
@@ -32,8 +34,7 @@ function PhotoBrowse({ photoList, onClose, prevPhotoBrowse, nextPhotoBrowse }: P
       switch (evt.dir) {
         case LEFT:
         case RIGHT:
-          let startLeft = (-window.innerWidth - 16 * 5)
-          _slider.style.left = startLeft + evt.deltaX + 'px'
+          _slider.style.left = CURR_PHOTO_LEFT() + evt.deltaX + 'px'
           break
         case UP:
         case DOWN:
@@ -42,7 +43,6 @@ function PhotoBrowse({ photoList, onClose, prevPhotoBrowse, nextPhotoBrowse }: P
       }
     },
     onSwiped: (evt) => {
-
       if (evt.velocity < 0.4) {
         if (evt.dir === LEFT || evt.dir === RIGHT) {
           if (evt.absX < window.innerWidth / 4) {
@@ -64,10 +64,10 @@ function PhotoBrowse({ photoList, onClose, prevPhotoBrowse, nextPhotoBrowse }: P
       }
 
       if (evt.dir === LEFT) {
-        slide('left', NEXT_PHOTO_LEFT, nextPhotoBrowse)
+        slide('left', NEXT_PHOTO_LEFT(), nextPhotoBrowse)
       }
       if (evt.dir === RIGHT) {
-        slide('left', PREV_PHOTO_LEFT, prevPhotoBrowse)
+        slide('left', PREV_PHOTO_LEFT(), prevPhotoBrowse)
       }
       if (evt.dir === UP) {
         slide('top', -window.innerHeight, onClose)
@@ -112,12 +112,12 @@ function PhotoBrowse({ photoList, onClose, prevPhotoBrowse, nextPhotoBrowse }: P
 
   return (
     <div className={'photo' + (photoIndex !== -1 ? '' : ' hide')} {...swipeHandlers}>
-      <div className="index">{photoIndex + 1 + '/' + photoList.length}</div>
       <div className="slider" ref={c => _slider = c as HTMLDivElement}>
         {hasBrowsed && !isFirst ? <img className="prev-img" src={api.image(photoList[photoIndex - 1].name)} crossOrigin="anonymous" /> : ''}
         {hasBrowsed ? <img className="curr-img" src={api.image(photoList[photoIndex].name)} crossOrigin="anonymous" /> : ''}
         {hasBrowsed && !isLast ? <img className="next-img" src={api.image(photoList[photoIndex + 1].name)} crossOrigin="anonymous" /> : ''}
       </div>
+      <div className="index">{photoIndex + 1 + '/' + photoList.length}</div>
       {photoList[photoIndex] ? <div className="file-name">{fileName()}</div> :  ''}
       <FontAwesomeIcon className="close" icon={faXmark} size="2x" onClick={onClose} />
     </div>
