fdf012d24b908f30f1812eb5d0fe83f41b3f61f8
Hubert Lu <goldbergrvd@gmail.com>
2022-04-29 14:38:38 +0800

    Photo browse slider transition zoom effect.

--- src/components/PhotoBrowse.tsx
+++ src/components/PhotoBrowse.tsx
@@ -14,17 +14,40 @@ export interface Props {
 
 let _slider: HTMLDivElement
 let _currImg: HTMLImageElement
+let _prevImg: HTMLImageElement
+let _nextImg: HTMLImageElement
 let swipeDir: 'vertical' | 'horizontal' | null = null
+let transitioning = false
 
 const FONT_SIZE = () => parseFloat(getComputedStyle(document.documentElement).fontSize)
-const PREV_PHOTO_SLIDER_LEFT = () => -window.innerWidth * 0 - FONT_SIZE() * 1
-const CURR_PHOTO_SLIDER_LEFT = () => -window.innerWidth * 1 - FONT_SIZE() * 3
-const NEXT_PHOTO_SLIDER_LEFT = () => -window.innerWidth * 2 - FONT_SIZE() * 5
-const CURR_IMAGE_WIDTH = (delta: number) => window.innerWidth * (0.7 + 0.3 * (window.innerWidth - delta) / window.innerWidth)
-const CURR_IMAGE_LEFT = (delta: number) => window.innerWidth + FONT_SIZE() * 3 + (window.innerWidth - CURR_IMAGE_WIDTH(delta)) / 2
-const TRANSITION_LEFT = 'left .1s ease'
-const TRANSITION_TOP = 'top .1s ease'
-const TRANSITION_TIME = 100
+const TRANSITION_SECOND = .5
+const SHRINK_WIDTH_RATIO = 0.7
+const SHRINK_GAP_RATIO = 1 - SHRINK_WIDTH_RATIO
+const CLOSE_WIDTH_RATIO = 0.2
+const CLOSE_GAP_RATIO = 1 - CLOSE_WIDTH_RATIO
+
+const BASE_PREV_LEFT = () => window.innerWidth * 0 + FONT_SIZE() * 1
+const BASE_CURR_LEFT = () => window.innerWidth * 1 + FONT_SIZE() * 3
+const BASE_NEXT_LEFT = () => window.innerWidth * 2 + FONT_SIZE() * 5
+
+const PREV_PHOTO_SLIDER_LEFT = () => -BASE_PREV_LEFT()
+const CURR_PHOTO_SLIDER_LEFT = () => -BASE_CURR_LEFT()
+const NEXT_PHOTO_SLIDER_LEFT = () => -BASE_NEXT_LEFT()
+
+const CURR_IMG_SLIDING_WIDTH = (delta: number) => window.innerWidth * SHRINK_WIDTH_RATIO + SHRINK_GAP_RATIO * (window.innerWidth - delta)
+const SIBLING_IMG_SLIDING_WIDTH = (delta: number) => window.innerWidth * SHRINK_WIDTH_RATIO + SHRINK_GAP_RATIO * delta
+const PREV_IMG_SLIDING_LEFT = (delta: number) => BASE_PREV_LEFT() + SHRINK_GAP_RATIO / 2 * delta
+const CURR_IMG_SLIDING_LEFT = (delta: number) => BASE_CURR_LEFT() + SHRINK_GAP_RATIO / 2 * delta
+const NEXT_IMG_SLIDING_LEFT = (delta: number) => BASE_NEXT_LEFT() + SHRINK_GAP_RATIO / 2 * delta
+
+const CURR_IMG_SLIDE_WIDTH = () => window.innerWidth * SHRINK_WIDTH_RATIO
+const CURR_IMG_SLIDE_LEFT = () => BASE_CURR_LEFT() + window.innerWidth * SHRINK_GAP_RATIO / 2
+const CURR_IMG_CLOSE_WIDTH = () => window.innerWidth * CLOSE_WIDTH_RATIO
+const CURR_IMG_CLOSE_LEFT = () => BASE_CURR_LEFT() + window.innerWidth * CLOSE_GAP_RATIO / 2
+
+const SIBLING_IMG_SLIDE_WIDTH = () => window.innerWidth
+const PREV_IMG_SLIDE_LEFT = () => BASE_PREV_LEFT()
+const NEXT_IMG_SLIDE_LEFT = () => BASE_NEXT_LEFT()
 
 
 function PhotoBrowse({ photoList, onClose, prevPhotoBrowse, nextPhotoBrowse }: Props) {
@@ -36,6 +59,7 @@ function PhotoBrowse({ photoList, onClose, prevPhotoBrowse, nextPhotoBrowse }: P
 
   const swipeHandlers = useSwipeable({
     onSwipeStart: (evt) => {
+      if (transitioning) return
       switch (evt.dir) {
         case LEFT:
         case RIGHT:
@@ -51,101 +75,168 @@ function PhotoBrowse({ photoList, onClose, prevPhotoBrowse, nextPhotoBrowse }: P
       switch (swipeDir) {
         case 'horizontal':
           _slider.style.left = CURR_PHOTO_SLIDER_LEFT() + evt.deltaX + 'px'
-          _currImg.style.width = CURR_IMAGE_WIDTH(evt.absX) + 'px'
-          _currImg.style.left = CURR_IMAGE_LEFT(evt.absX) + 'px'
+
+          _currImg.style.width = CURR_IMG_SLIDING_WIDTH(evt.absX) + 'px'
+          _currImg.style.left = CURR_IMG_SLIDING_LEFT(evt.absX) + 'px'
+
+          if (_prevImg) {
+            _prevImg.style.width = SIBLING_IMG_SLIDING_WIDTH(evt.absX) + 'px'
+            _prevImg.style.left = PREV_IMG_SLIDING_LEFT(evt.absX) + 'px'
+          }
+
+          if (_nextImg) {
+            _nextImg.style.width = SIBLING_IMG_SLIDING_WIDTH(evt.absX) + 'px'
+            _nextImg.style.left = NEXT_IMG_SLIDING_LEFT(evt.absX) + 'px'
+          }
           break
+
         case 'vertical':
           _currImg.style.top = evt.deltaY + 'px'
-          _currImg.style.width = CURR_IMAGE_WIDTH(evt.absY) + 'px'
-          _currImg.style.left = CURR_IMAGE_LEFT(evt.absY) + 'px'
+          _currImg.style.width = CURR_IMG_SLIDING_WIDTH(evt.absY) + 'px'
+          _currImg.style.left = CURR_IMG_SLIDING_LEFT(evt.absY) + 'px'
           break
       }
     },
     onSwiped: (evt) => {
+      let restXPercentage = (window.innerWidth - evt.absX) / window.innerWidth
+      let restYPercentage = (window.innerHeight - evt.absY) / window.innerHeight
+
       if (evt.velocity < 0.4) {
         if (swipeDir === 'horizontal') {
-          if (evt.absX < window.innerWidth / 4) {
-            revertSlider()
+          if (restXPercentage > .6) {
+            revertSlider(restXPercentage)
             return
           }
         }
         if (swipeDir === 'vertical') {
-          if (evt.absY < window.innerHeight / 5) {
-            revertSlider()
+          if (restYPercentage > .8) {
+            revertSlider(restYPercentage)
             return
           }
         }
       }
 
       if (swipeDir === 'horizontal') {
-        if (evt.dir === LEFT && isLast || evt.dir === RIGHT && isFirst) {
-          revertSlider()
+        if ((evt.dir === LEFT && isLast) || (evt.dir === RIGHT && isFirst)) {
+          revertSlider(restXPercentage)
           return
         }
 
         if (evt.dir === LEFT) {
-          slideHorizontal(NEXT_PHOTO_SLIDER_LEFT(), nextPhotoBrowse)
+          slideHorizontal(NEXT_PHOTO_SLIDER_LEFT(), restXPercentage, nextPhotoBrowse)
           return
         }
         if (evt.dir === RIGHT) {
-          slideHorizontal(PREV_PHOTO_SLIDER_LEFT(), prevPhotoBrowse)
+          slideHorizontal(PREV_PHOTO_SLIDER_LEFT(), restXPercentage, prevPhotoBrowse)
           return
         }
-        revertSlider()
+        revertSlider(restXPercentage)
       }
 
       if (swipeDir === 'vertical') {
         if (evt.dir === UP) {
-          slideVertical(-window.innerHeight)
+          slideVertical(-window.innerHeight, restYPercentage)
           return
         }
         if (evt.dir === DOWN) {
-          slideVertical(window.innerHeight)
+          slideVertical(window.innerHeight, restYPercentage)
           return
         }
-        revertSlider()
+        revertSlider(restYPercentage)
       }
     }
   })
 
-  function slideHorizontal(val: number, callback: () => void) {
-    _slider.style.transition = TRANSITION_LEFT
-    _slider.style.left = val + 'px'
-    swipeDir = null
-    setTimeout(() => {
+  function slideHorizontal(sliderLeft: number, restPercentage: number, callback: () => void) {
+    let transitionTime = TRANSITION_SECOND * restPercentage
+    _slider.style.transition = `left ${transitionTime}s ease`
+    _slider.style.left = sliderLeft + 'px'
+
+    Array.from([_currImg, _prevImg, _nextImg]).forEach(e => {
+      if (e) {
+        e.style.transition = `all ${transitionTime}s ease`
+        e.style.transitionProperty = 'width, left'
+        if (e === _currImg) {
+          _currImg.style.width = CURR_IMG_SLIDE_WIDTH() + 'px'
+          _currImg.style.left = CURR_IMG_SLIDE_LEFT() + 'px'
+        }
+        if (e === _prevImg) {
+          _prevImg.style.width = SIBLING_IMG_SLIDE_WIDTH() + 'px'
+          _prevImg.style.left = PREV_IMG_SLIDE_LEFT() + 'px'
+        }
+        if (e === _nextImg) {
+          _nextImg.style.width = SIBLING_IMG_SLIDE_WIDTH() + 'px'
+          _nextImg.style.left = NEXT_IMG_SLIDE_LEFT() + 'px'
+        }
+      }
+    })
+
+    waitTransition(() => {
       callback()
       _slider.style.transition = ''
       _slider.style.left = ''
-      _currImg.style.width = ''
-      _currImg.style.left = ''
-    }, TRANSITION_TIME)
+
+      Array.from([_currImg, _prevImg, _nextImg]).forEach(e => {
+        if (e) {
+          e.style.transition = ''
+          e.style.transitionProperty = ''
+          e.style.width = ''
+          e.style.left = ''
+        }
+      })
+    }, transitionTime * 1000)
   }
 
-  function slideVertical(val: number) {
-    _currImg.style.transition = TRANSITION_TOP
-    _currImg.style.top = val + 'px'
-    swipeDir = null
-    setTimeout(() => {
+  function slideVertical(imgTop: number, restPercentage: number) {
+    let transitionTime = TRANSITION_SECOND * restPercentage
+    _currImg.style.transition = `all ${transitionTime}s ease`
+    _currImg.style.transitionProperty = 'width, left, top'
+    _currImg.style.width = CURR_IMG_CLOSE_WIDTH() + 'px'
+    _currImg.style.left = CURR_IMG_CLOSE_LEFT() + 'px'
+    _currImg.style.top = imgTop + 'px'
+
+    waitTransition(() => {
       onClose()
-    }, TRANSITION_TIME)
+    }, transitionTime * 1000)
   }
 
-  function revertSlider() {
+  function revertSlider(restPercentage: number) {
+    let transitionTime = TRANSITION_SECOND * (1 - restPercentage)
     if (swipeDir === 'horizontal') {
-      _slider.style.transition = TRANSITION_LEFT
+      _slider.style.transition = `left ${transitionTime}s ease`
       _slider.style.left = ''
     }
-    if (swipeDir === 'vertical') {
-      _currImg.style.transition = TRANSITION_TOP
-      _currImg.style.top = ''
-    }
+
+    _currImg.style.transition = `all ${transitionTime}s ease`
+    _currImg.style.transitionProperty = 'width, left, top'
     _currImg.style.width = ''
     _currImg.style.left = ''
-    swipeDir = null
-    setTimeout(() => {
+    _currImg.style.top = ''
+
+    waitTransition(() => {
       _slider.style.transition = ''
+
       _currImg.style.transition = ''
-    }, TRANSITION_TIME)
+      _currImg.style.transitionProperty = ''
+
+      if (_prevImg) {
+        _prevImg.style.width = ''
+        _prevImg.style.left = ''
+      }
+      if (_nextImg) {
+        _nextImg.style.width = ''
+        _nextImg.style.left = ''
+      }
+    }, transitionTime * 1000)
+  }
+
+  function waitTransition(afterTransition: () => void, ms: number) {
+    swipeDir = null
+    transitioning = true
+    setTimeout(() => {
+      transitioning = false
+      afterTransition()
+    }, ms)
   }
 
   function fileName() {
@@ -165,9 +256,27 @@ function PhotoBrowse({ photoList, onClose, prevPhotoBrowse, nextPhotoBrowse }: P
   return (
     <div className={'photo' + (photoIndex !== -1 ? '' : ' hide')} {...swipeHandlers}>
       <div className="slider" ref={c => _slider = c as HTMLDivElement}>
-        {hasBrowsed && !isFirst ? <img className="prev-img" src={api.image(photoList[photoIndex - 1].name)} crossOrigin="anonymous" /> : ''}
-        {hasBrowsed ? <img className="curr-img" src={api.image(photoList[photoIndex].name)} crossOrigin="anonymous" ref={c => _currImg = c as HTMLImageElement} /> : ''}
-        {hasBrowsed && !isLast ? <img className="next-img" src={api.image(photoList[photoIndex + 1].name)} crossOrigin="anonymous" /> : ''}
+        {
+          hasBrowsed && !isFirst
+          ?
+          <img className="prev-img" src={api.image(photoList[photoIndex - 1].name)} crossOrigin="anonymous" alt="" ref={c => _prevImg = c as HTMLImageElement} />
+          :
+          ''
+        }
+        {
+          hasBrowsed
+          ?
+          <img className="curr-img" src={api.image(photoList[photoIndex].name)} crossOrigin="anonymous" alt="" ref={c => _currImg = c as HTMLImageElement} />
+          :
+          ''
+        }
+        {
+          hasBrowsed && !isLast
+          ?
+          <img className="next-img" src={api.image(photoList[photoIndex + 1].name)} crossOrigin="anonymous" alt="" ref={c => _nextImg = c as HTMLImageElement} />
+          :
+          ''
+        }
       </div>
       <div className="index">{photoIndex + 1 + '/' + photoList.length}</div>
       {photoList[photoIndex] ? <div className="file-name">{fileName()}</div> :  ''}
